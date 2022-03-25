44267b43ebe2f820f7b166c74535bb443adb78be
shoronshaha <smaranshaha@gmail.com>
2022-03-25 23:40:15 +0600

    header added

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Online <span className='guns'>Guns</span> Shop</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="guns-container">
+
+
+            </div>
+            <div className="cart-container">
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
