e21a6b2715925317d7ea491c1e2d5e803fd2eaaf
shoronshaha <smaranshaha@gmail.com>
2022-03-26 13:26:25 +0600

    qus-ans updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/Header';
 import Shop from './components/shop/Shop';
+import Utilities from './components/utilities/Utilities';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <Utilities></Utilities>
     </div>
   );
 }
---
+++ src/components/utilities/Utilities.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Utilities.css'
+const Utilities = () => {
+    return (
+        <div >
+            <div className='qus-ans'>
+                <h1>1. How React works ?</h1>
+                <p>রিয়্যাক্ট আমাদের যে Real dom আছে সেটারই একটা virtual dom  ক্রিয়েট করে রাখে।
+                    আমরা যখনই আমাদের Real dom এ চেন্জ করি না কেন, সে তার virtual dom যেটা রেখে দিচ্ছে সে তার সাথে
+                    real dom এর compere করে দেখে চেন্জ কোথায় কোথায় হয়েছে এবং সেটা রিপ্লেস করে update করে browser
+                    এ দেখায়।
+                </p>
+            </div>
+            <div className='qus-ans'>
+                <h1>2. State Vs Props </h1>
+                <p> <h3>State</h3>
+                    The state is an updatable structure that is used to contain data or
+                    information about the component and can change over time. The change in state
+                    can happen as a response to user action or system event. It is the heart of the react
+                    component which determines the behavior of the component and how it will render.
+                    A state must be kept as simple as possible. It represents the component's local state or information.
+                    It can only be accessed or modified inside the component or by the component directly.</p>
+
+                <p> <h3> Props</h3>
+                    Props are read-only components. It is an object which stores the value of attributes of a tag and work similar
+                    to the HTML attributes. It allows passing data from one component to other components.
+                    It is similar to function arguments and can be passed to the
+                    component the same way as arguments passed in a function. Props are immutable so we
+                    cannot modify the props from inside the component.</p>
+            </div>
+            <div className='qus-ans'>
+                <h1>3. How useState works ?</h1>
+                <p>useState is a Hook that allows you to have state variables in functional components.
+                    You pass the initial state to this function and it returns a variable with the current state value
+                    (not necessarily the initial state) and another function to update this value.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Utilities;
